8158eaae6bebb8529fac6a4089cc540df5d7c206
Abinaya8543 <abinayaabinaya90118@gmail.com>
2024-09-03 16:28:04 +0530

    initial commit

---
+++ variasble/practice.js
@@ -0,0 +1,26 @@
+let r= 7;
+let pi=22.7;
+let areaOfCircle= pi*r*r;
+let area=22.7*(7**2);
+console.log("areaOfCircle"+area);
+
+
+let profitPresentage=10;
+let costPrice=500;
+let sellingPrice=500+(500*10/100);
+let actualPrice =costPrice+(profitPercentage*costPrice);
+console.log("price"+actualPrice);
+
+let a=10;
+let b=8;
+let c=4;
+// let totalTrees=10+8+4;
+let totalNoOfTrees=a+b+c;
+console.log("trees"+totalNotrees);
+
+
+
+let A=5;
+let r3=1.732;
+let equitri=1/4*(1.732*5*5);
+console.log(equicdtri);
